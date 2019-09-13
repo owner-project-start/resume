0d4fd82e8248f11815619f282d64e2508212ae3e
NgoUyUong <ngouyoung@yahoo.com>
2019-09-13 16:22:48 +0700

    commit

---
+++ database/migrations/2019_09_13_090044_create_experiences_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExperiencesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->string('position');
+            $table->string('company');
+            $table->string('content');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+}
---
+++ resources/views/partials/script.blade.php
@@ -0,0 +1,9 @@
+<!-- Bootstrap core JavaScript -->
+<script src="{{ asset('node_modules/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Plugin JavaScript -->
+<script src="{{ asset('node_modules/jquery.easing/jquery.easing.min.js') }}"></script>
+
+<!-- Custom scripts for this template -->
+<script src="{{ asset('js/resume.min.js') }}"></script>
