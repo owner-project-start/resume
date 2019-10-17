12a9898e4ef1d9a9527e877df4ce70322d4908e8
@UyUong <ngouyoung@yahoo.com>
2019-10-17 10:11:23 +0700

    update code

--- routes/Web/social.php
+++ routes/Web/social.php
@@ -2,8 +2,8 @@
 
 Route::group(['prefix' => 'socials'], function () {
     Route::get('', 'SocialController@index')->name('socials');
-    Route::get('getList', 'SocialController@all')->name('socials.getList');
-    Route::post('getById', 'SocialController@getById')->name('socials.getById');
-    Route::post('store', 'SocialController@store')->name('socials.store');
-    Route::post('delete', 'SocialController@destroy')->name('socials.delete');
+//    Route::get('getList', 'SocialController@all')->name('socials.getList');
+//    Route::post('getById', 'SocialController@getById')->name('socials.getById');
+//    Route::post('store', 'SocialController@store')->name('socials.store');
+//    Route::post('delete', 'SocialController@destroy')->name('socials.delete');
 });
