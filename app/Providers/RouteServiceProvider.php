ff2b38406c83f98ec87af3170c4468842c371d7f
@UyUong <ngouyoung@yahoo.com>
2019-10-17 11:58:38 +0700

    update

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -51,9 +51,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('Web')
+        Route::middleware('web')
              ->namespace($this->namespace)
-             ->group(base_path('routes/Web.php'));
+             ->group(base_path('routes/web.php'));
     }
 
     /**
